96b33d3d98e059dde64b05811fae6d2bb40e7051
Ugur Guney <ugurguney@gmail.com>
2017-01-23 08:12:19 -0500

    init project

---
+++ api.py
@@ -0,0 +1,76 @@
+import json
+
+from flask import Flask, request, render_template, abort, jsonify
+import flask_sqlalchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survey-results.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Unicode)
+    correct = db.Column(db.Boolean)
+    question_no = db.Column(db.Integer)
+    answer = db.Column(db.Unicode)
+
+    def __init__(self, email, correct, question_no, answer):
+        self.email = email
+        self.correct = correct
+        self.question_no = question_no
+        self.answer = answer
+
+    def to_dict(self):
+        return {
+            'email': self.email,
+            'correct': self.correct,
+            'question_no': self.question_no,
+            'answer': self.answer
+        }
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+KEYS = ['email', 'correct', 'question_no', 'answer']
+
+@app.route('/questions/<int:qa_id>')
+def questions(qa_id):
+    with app.open_resource('static/survey-data/{}.json'.format(qa_id), 'r') as f:
+        contents = json.load(f)
+    return json.dumps(contents)
+
+@app.route('/answers', methods=['GET', 'POST'])
+def responses():
+    if request.method == 'GET':
+        return get_all_answers()
+
+    req = request.get_json()
+    for key in KEYS:
+        if key not in req:
+            return 'missing key "{}"'.format(key), 400
+    email = req['email']
+    correct = req['correct']
+    question_no = req['question_no']
+    answer_str = req['answer']
+    print(email, correct, question_no, answer_str)
+    # print(type(email), type(correct), type(question_no), type(answer_str))
+    answer = Answer(email, correct, question_no, answer_str)
+    db.session.add(answer)
+    db.session.commit()
+    return 'saved {}th answer'.format(Answer.query.count())
+
+
+def get_all_answers():
+    answers = Answer.query.all()
+    answer_dicts = [answer.to_dict() for answer in answers]
+    return jsonify(answer_dicts), 200
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True, port=7000)
---
+++ generate_survey_from_testdata.py
@@ -0,0 +1,62 @@
+"""
+Script to prepare the QA-search test dataset for human testing by creating a
+JSON file for each question to be used by a web-based survey.
+"""
+import os
+import json
+
+DATASET_FILE = r'C:\Users\veliu\Downloads\test_no_candidates_unigram_question.txt'
+OUTPUT_FOLDER = r'C:\Users\veliu\Documents\GitRepos\qasearch-survey\static\survey-data'
+
+def main():
+    print('Starting...')
+    with open(DATASET_FILE, 'rt') as f:
+        lines = f.readlines()
+    print('{} lines read from dataset file.'.format(len(lines)))
+
+    chunks = split_list_into_chunks(lines)
+    print('{} chunks separted via empty lines.'.format(len(chunks)))
+
+    for out_no, chunk in enumerate(chunks):
+        output_file = '{n}.json'.format(n=out_no)
+        qa = convert_chunk_to_qa(chunk)
+        output_path = os.path.join(OUTPUT_FOLDER, output_file)
+        with open(output_path, 'wt') as out:
+            json.dump(qa, out, indent=1)
+    print('Chunks have been converted to JSON files.')
+
+def split_list_into_chunks(lines):
+    """Group lines of unigram dataset into individual qa parts.
+
+    Read lines of the dataset file into a list. Split list between lines that
+    are empty."""
+    qas = []
+    qa = []
+    for line in lines:
+        if line == '\n':
+            qas.append(qa)
+            qa = []
+            continue
+        qa.append(line[:-1])  # remove '\n' at the end of each line
+    return qas
+
+
+def convert_chunk_to_qa(chunk):
+    """Parse a chunk's list into a dictionary."""
+    snippet_lines = chunk[:-1]
+    question_answer_line = chunk[-1]
+    snippets = [line.split(' ', 1)[1] for line in snippet_lines]  # rest after the first space
+    question_answer_line = question_answer_line.split(' ', 1)[1]  # rest after the first space
+    splitted = question_answer_line.split('\t')
+    question = splitted[0]
+    answer = splitted[1]
+    d = {
+        'snippets': snippets,
+        'question': question,
+        'answer': answer
+    }
+    return d
+
+
+if __name__ == '__main__':
+    main()
